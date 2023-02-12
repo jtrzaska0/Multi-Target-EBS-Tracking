e5870a2bbdc75214a0908e8c576f3bd78f5c8f9b
Trevor Schlack <schlackt@gmail.com>
2023-02-12 01:15:36 -0700

    Add support for DAVIS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <fstream>
 
 #include <libcaercpp/devices/dvxplorer.hpp>
+#include <libcaercpp/devices/davis.hpp>
 
 #include <atomic>
 #include <csignal>
+#include <tuple>
 #include <queue>
 
 #include <opencv2/core.hpp>
@@ -133,11 +135,11 @@ void plot_events(double dt, double mag, int Nx, int Ny, bool& active) {
     cv::destroyWindow("PLOT_EVENTS");
 }
 
-void read_packets(bool& active) {
+void read_packets(int Nx, int Ny, bool& active) {
     while (active) {
         while (!PlottingPacketQueue.empty() && active) {
             auto polarity = PlottingPacketQueue.front();
-            cv::Mat cvEvents(480, 640, CV_8UC3, cv::Vec3b{127, 127, 127});
+            cv::Mat cvEvents(Ny, Nx, CV_8UC3, cv::Vec3b{127, 127, 127});
             for (const auto &e : *polarity) {
                 cvEvents.at<cv::Vec3b>(e.getY(), e.getX())
                     = e.getPolarity() ? cv::Vec3b{255, 255, 255} : cv::Vec3b{0, 0, 0};
@@ -149,7 +151,7 @@ void read_packets(bool& active) {
     }
 }
 
-int init_xplorer(bool& active) {
+std::tuple<int16_t, int16_t, libcaer::devices::davis> init_davis() {
     // Install signal handler for global shutdown.
 #if defined(_WIN32)
     if (signal(SIGTERM, &globalShutdownSignalHandler) == SIG_ERR) {
@@ -175,13 +177,93 @@ int init_xplorer(bool& active) {
     if (sigaction(SIGTERM, &shutdownAction, nullptr) == -1) {
         libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
                           "Failed to set signal handler for SIGTERM. Error: %d.", errno);
-        return (EXIT_FAILURE);
     }
 
     if (sigaction(SIGINT, &shutdownAction, nullptr) == -1) {
         libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
                           "Failed to set signal handler for SIGINT. Error: %d.", errno);
-        return (EXIT_FAILURE);
+    }
+#endif
+
+    // Open a DAVIS, give it a device ID of 1, and don't care about USB bus or SN restrictions.
+    libcaer::devices::davis davisHandle = libcaer::devices::davis(1);
+
+    // Let's take a look at the information we have on the device.
+    struct caer_davis_info davis_info = davisHandle.infoGet();
+
+    printf("%s --- ID: %d, Master: %d, DVS X: %d, DVS Y: %d, Logic: %d.\n", davis_info.deviceString,
+           davis_info.deviceID, davis_info.deviceIsMaster, davis_info.dvsSizeX, davis_info.dvsSizeY,
+           davis_info.logicVersion);
+
+    // Send the default configuration before using the device.
+    // No configuration is sent automatically!
+    davisHandle.sendDefaultConfig();
+
+    // Tweak some biases, to increase bandwidth in this case.
+    struct caer_bias_coarsefine coarseFineBias{};
+
+    coarseFineBias.coarseValue        = 2;
+    coarseFineBias.fineValue          = 116;
+    coarseFineBias.enabled            = true;
+    coarseFineBias.sexN               = false;
+    coarseFineBias.typeNormal         = true;
+    coarseFineBias.currentLevelNormal = true;
+
+    davisHandle.configSet(DAVIS_CONFIG_BIAS, DAVIS240_CONFIG_BIAS_PRBP, caerBiasCoarseFineGenerate(coarseFineBias));
+
+    coarseFineBias.coarseValue        = 1;
+    coarseFineBias.fineValue          = 33;
+    coarseFineBias.enabled            = true;
+    coarseFineBias.sexN               = false;
+    coarseFineBias.typeNormal         = true;
+    coarseFineBias.currentLevelNormal = true;
+
+    davisHandle.configSet(DAVIS_CONFIG_BIAS, DAVIS240_CONFIG_BIAS_PRSFBP, caerBiasCoarseFineGenerate(coarseFineBias));
+
+    // Let's verify they really changed!
+    uint32_t prBias   = davisHandle.configGet(DAVIS_CONFIG_BIAS, DAVIS240_CONFIG_BIAS_PRBP);
+    uint32_t prsfBias = davisHandle.configGet(DAVIS_CONFIG_BIAS, DAVIS240_CONFIG_BIAS_PRSFBP);
+
+    printf("New bias values --- PR-coarse: %d, PR-fine: %d, PRSF-coarse: %d, PRSF-fine: %d.\n",
+           caerBiasCoarseFineParse(prBias).coarseValue, caerBiasCoarseFineParse(prBias).fineValue,
+           caerBiasCoarseFineParse(prsfBias).coarseValue, caerBiasCoarseFineParse(prsfBias).fineValue);
+
+    std::tuple<int16_t, int16_t, libcaer::devices::davis> ret = {davis_info.dvsSizeX, davis_info.dvsSizeY, davisHandle};
+
+    return ret;
+}
+
+std::tuple<int16_t, int16_t, libcaer::devices::dvXplorer> init_xplorer() {
+    // Install signal handler for global shutdown.
+#if defined(_WIN32)
+    if (signal(SIGTERM, &globalShutdownSignalHandler) == SIG_ERR) {
+		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+			"Failed to set signal handler for SIGTERM. Error: %d.", errno);
+		return (EXIT_FAILURE);
+	}
+
+	if (signal(SIGINT, &globalShutdownSignalHandler) == SIG_ERR) {
+		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+			"Failed to set signal handler for SIGINT. Error: %d.", errno);
+		return (EXIT_FAILURE);
+	}
+#else
+    struct sigaction shutdownAction{};
+
+    shutdownAction.sa_handler = &globalShutdownSignalHandler;
+    shutdownAction.sa_flags   = 0;
+    sigemptyset(&shutdownAction.sa_mask);
+    sigaddset(&shutdownAction.sa_mask, SIGTERM);
+    sigaddset(&shutdownAction.sa_mask, SIGINT);
+
+    if (sigaction(SIGTERM, &shutdownAction, nullptr) == -1) {
+        libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+                          "Failed to set signal handler for SIGTERM. Error: %d.", errno);
+    }
+
+    if (sigaction(SIGINT, &shutdownAction, nullptr) == -1) {
+        libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+                          "Failed to set signal handler for SIGINT. Error: %d.", errno);
     }
 #endif
 
@@ -197,7 +279,58 @@ int init_xplorer(bool& active) {
     // Send the default configuration before using the device.
     // No configuration is sent automatically!
     handle.sendDefaultConfig();
+    std::tuple<int16_t, int16_t, libcaer::devices::dvXplorer> ret = {info.dvsSizeX, info.dvsSizeY, handle};
 
+    return ret;
+}
+
+int read_xplorer (const libcaer::devices::dvXplorer& handle, bool& active) {
+    // Now let's get start getting some data from the device. We just loop in blocking mode,
+    // no notification needed regarding new events. The shutdown notification, for example if
+    // the device is disconnected, should be listened to.
+    handle.dataStart(nullptr, nullptr, nullptr, &usbShutdownHandler, nullptr);
+
+    // Let's turn on blocking data-get mode to avoid wasting resources.
+    handle.configSet(CAER_HOST_CONFIG_DATAEXCHANGE, CAER_HOST_CONFIG_DATAEXCHANGE_BLOCKING, true);
+
+    while (!globalShutdown.load(std::memory_order_relaxed) && active) {
+        std::unique_ptr<libcaer::events::EventPacketContainer> packetContainer = handle.dataGet();
+        if (packetContainer == nullptr) {
+            continue; // Skip if nothing there.
+        }
+
+        for (auto &packet: *packetContainer) {
+            if (packet == nullptr) {
+                //printf("Packet is empty (not present).\n");
+                continue; // Skip if nothing there.
+            }
+
+            if (packet->getEventType() == POLARITY_EVENT) {
+                std::shared_ptr<const libcaer::events::PolarityEventPacket> polarity
+                        = std::static_pointer_cast<libcaer::events::PolarityEventPacket>(packet);
+
+                std::vector<double> events;
+                PlottingPacketQueue.push(polarity);
+
+                for (const auto &e : *polarity) {
+                    events.push_back((double)e.getTimestamp()/1000);
+                    events.push_back(e.getX());
+                    events.push_back(e.getY());
+                    events.push_back(e.getPolarity());
+                }
+                TrackingVectorQueue.push(events);
+            }
+        }
+    }
+    handle.dataStop();
+
+    // Close automatically done by destructor.
+    printf("Shutdown successful.\n");
+
+    return (EXIT_SUCCESS);
+}
+
+int read_davis (const libcaer::devices::davis& handle, bool& active) {
     // Now let's get start getting some data from the device. We just loop in blocking mode,
     // no notification needed regarding new events. The shutdown notification, for example if
     // the device is disconnected, should be listened to.
@@ -253,31 +386,53 @@ bool key_is_pressed(KeySym ks) {
     return isPressed;
 }
 
+void runner(std::thread& reader, std::thread& plotter, std::thread& tracker, std::thread& imager, bool& active) {
+    printf("Press space to stop...\n");
+    int i = 0;
+    while(active) {
+        if (key_is_pressed(XK_space)) {
+            active = false;
+        }
+        if (i > 20) {
+            i = 0;
+            printf("Queue Sizes:\n");
+            printf("------------\n");
+            printf("Plotting Queue: %zu\n", PlottingPacketQueue.size());
+            printf("Event Vector Queue: %zu\n", TrackingVectorQueue.size());
+            printf("Image Matrix Queue: %zu\n", CVMatrixQueue.size());
+            printf("Position Vector Queue: %zu\n\n\n", PositionsVectorQueue.size());
+        }
+        i += 1;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    reader.join();
+    plotter.join();
+    tracker.join();
+    imager.join();
+}
+
 int main(int argc, char* argv[]) {
     /*
      Simulate live data tracking using a CSV file containing event data.
      A separate Matlab script can be used to generate GIFs of the result.
 
      Args:
-          argv[1]: Integration time in milliseconds.
-          argv[2]: Magnification
-          argv[3]: Number of horizontal pixels
-          argv[4]: Number of vertical pixels
+          argv[1]: Device type: "xplorer" or "davis"
+          argv[2]: Integration time in milliseconds.
+          argv[3]: Magnification
 
      Ret:
           0
      */
 
-    if (argc != 5) {
+    if (argc != 4) {
         printf("Invalid number of arguments.\n");
         return 0;
     }
 
-    // Integration time in ms
-    double integrationtime = {std::stod(argv[1])};
-    double mag = {std::stod(argv[2])};
-    int Nx = {std::stoi(argv[3])};
-    int Ny = {std::stoi(argv[4])};
+    std::string device_tpye = {std::string(argv[1])};
+    double integrationtime = {std::stod(argv[2])};
+    double mag = {std::stod(argv[3])};
 
     /**Create an Algorithm object here.**/
     // Matrix initializer
@@ -308,35 +463,33 @@ int main(int argc, char* argv[]) {
     // Algo initializer
     DBSCAN_KNN algo(invals, k_model);
 
+
     bool active = true;
-    std::thread writing_thread(init_xplorer, std::ref(active));
-    std::thread plotting_thread(read_packets, std::ref(active));
-    std::thread tracking_thread(tracker, integrationtime, algo, std::ref(active));
-    std::thread image_thread(plot_events, integrationtime, mag, Nx, Ny, std::ref(active));
 
-    printf("Press space to stop...\n");
-    int i = 0;
-    while(active) {
-        if (key_is_pressed(XK_space)) {
-            active = false;
-        }
-        if (i > 20) {
-            i = 0;
-            printf("Queue Sizes:\n");
-            printf("------------\n");
-            printf("Plotting Queue: %zu\n", PlottingPacketQueue.size());
-            printf("Event Vector Queue: %zu\n", TrackingVectorQueue.size());
-            printf("Image Matrix Queue: %zu\n", CVMatrixQueue.size());
-            printf("Position Vector Queue: %zu\n\n\n", PositionsVectorQueue.size());
-        }
-        i += 1;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    if (device_tpye == "xplorer") {
+        auto params = init_xplorer();
+        int Nx = std::get<0>(params);
+        int Ny = std::get<1>(params);
+        auto handle = std::get<2>(params);
+        std::thread plotting_thread(read_packets, Nx, Ny, std::ref(active));
+        std::thread tracking_thread(tracker, integrationtime, algo, std::ref(active));
+        std::thread image_thread(plot_events, integrationtime, mag, Nx, Ny, std::ref(active));
+        std::thread writing_thread(read_xplorer, std::ref(handle), std::ref(active));
+        std::thread running_thread(runner, std::ref(writing_thread), std::ref(plotting_thread), std::ref(tracking_thread), std::ref(image_thread), std::ref(active));
+        running_thread.join();
     }
-
-    writing_thread.join();
-    plotting_thread.join();
-    tracking_thread.join();
-    image_thread.join();
+    else {
+        auto params = init_davis();
+        int Nx = std::get<0>(params);
+        int Ny = std::get<1>(params);
+        auto handle = std::get<2>(params);
+        std::thread plotting_thread(read_packets, Nx, Ny, std::ref(active));
+        std::thread tracking_thread(tracker, integrationtime, algo, std::ref(active));
+        std::thread image_thread(plot_events, integrationtime, mag, Nx, Ny, std::ref(active));
+        std::thread writing_thread(read_davis, std::ref(handle), std::ref(active));
+        std::thread running_thread(runner, std::ref(writing_thread), std::ref(plotting_thread), std::ref(tracking_thread), std::ref(image_thread), std::ref(active));
+        running_thread.join();
+    }
 
     return 0;
 }
