d469ff0fa5a979241b9e555b84591fe3d7a0f8aa
Trevor Schlack <schlackt@gmail.com>
2023-06-05 14:52:54 -0700

    Fix DBSCAN parameters

--- main.cpp
+++ main.cpp
@@ -96,40 +96,25 @@ int main(int argc, char *argv[]) {
     double nfov_px_size = stage_params.value("NFOV_PX_PITCH", 0.0000022);
     Buffers buffers(history_size);
 
-    /**Create an Algorithm object here.**/
-    // Matrix initializer
     // DBSCAN
-    Eigen::MatrixXd invals{Eigen::MatrixXd::Zero(1, 4)};
+    Eigen::MatrixXd invals {Eigen::MatrixXd::Zero(1, 3)};
+    invals(0, 0) = 8;
+    invals(0, 1) = 8;
+    invals(0, 2) = 1.2;
 
-    // Mean Shift
-    invals(0, 0) = 5.2;
-    invals(0, 1) = 9;
-    invals(0, 2) = 74;
-    invals(0, 3) = 1.2;
     // Model initializer
     double DT = integrationtime;
     double p3 = pow(DT, 3) / 3;
     double p2 = pow(DT, 2) / 2;
 
-    Eigen::MatrixXd P{{16, 0,  0, 0},
-                      {0,  16, 0, 0},
-                      {0,  0,  9, 0},
-                      {0,  0,  0, 9}};
-    Eigen::MatrixXd F{{1, 0, DT, 0},
-                      {0, 1, 0,  DT},
-                      {0, 0, 1,  0},
-                      {0, 0, 0,  1}};
-    Eigen::MatrixXd Q{{p3, 0,  p2, 0},
-                      {0,  p3, 0,  p2},
-                      {p2, 0,  DT, 0},
-                      {0,  p2, 0,  DT}};
-    Eigen::MatrixXd H{{1, 0, 0, 0},
-                      {0, 1, 0, 0}};
-    Eigen::MatrixXd R{{7, 0},
-                      {0, 7}};
+    Eigen::MatrixXd P {{16, 0, 0, 0}, {0, 16, 0, 0}, {0, 0, 9, 0}, {0, 0, 0, 9}};
+    Eigen::MatrixXd F {{1, 0, DT, 0}, {0, 1, 0, DT}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+    Eigen::MatrixXd Q {{p3, 0, p2, 0}, {0, p3, 0, p2}, {p2, 0, DT, 0}, {0, p2, 0, DT}};
+    Eigen::MatrixXd H {{1, 0, 0, 0}, {0, 1, 0, 0}};
+    Eigen::MatrixXd R {{7, 0}, {0, 7}};
 
-    // Define the model.
-    KModel k_model{.dt = DT, .P = P, .F = F, .Q = Q, .H = H, .R = R};
+    // Define the model for KNN.
+    KModel k_model {.dt = DT, .P = P, .F = F, .Q = Q, .H = H, .R = R};
     // Algo initializer
     DBSCAN_KNN algo(invals, k_model);
 
